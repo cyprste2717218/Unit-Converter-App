005f2ff382c348ea569f220c7e7cc6de6d98aadf
Stephen Ingham <stephenmichael833@gmail.com>
2023-01-20 01:09:52 +0000

    -installed new package 'react-google-material-icons' and set up icons for all unit categories,  ... ,-set up credit text in footer linking to my github, ... ,-changed text font size to em units, ... ,-installed and replaced old font with 'Work Sans', ... ,-updated title of webpage to 'Unit Converter App' & described the app in the meta tag for type description, ... ,-centered text in footer, ...,-replaced placeholder text in swap units box with swap symbol, ...,-updated the 'radios' array to include the name of each icon to be substituted into map function in ButtonGroup element (being used within icon attribute of MaterialIcon element)

--- unit-converter-app/src/App.js
+++ unit-converter-app/src/App.js
@@ -11,6 +11,7 @@ import Carousel from 'react-bootstrap/Carousel';
 import Button from 'react-bootstrap/Button';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 import Dropdown from 'react-bootstrap/Dropdown';
+import MaterialIcon from 'react-google-material-icons';
 
 
 function App() {
@@ -19,18 +20,18 @@ function App() {
   const [radioValue, setRadioValue] = useState('1');
 
   const radios = [
-    { name: 'Temperature', value: '1' },
-    { name: 'Length', value: '2' },
-    { name: 'Area', value: '3' },
-    { name: 'Weight', value: '4' },
-    { name: 'Volume', value: '5' },
-    { name: 'Currency', value: '6' },
-    { name: 'Time', value: '7' },
-    { name: 'Speed', value: '8' },
-    { name: 'Power', value: '9' },
-    { name: 'Angle', value: '10' },
-    { name: 'Data', value: '11' },
-    { name: 'Fuel', value: '12' },
+    { name: 'Temperature', value: '1', icon: "device_thermostat" },
+    { name: 'Length', value: '2', icon: "square_foot" },
+    { name: 'Area', value: '3', icon: "crop_square" },
+    { name: 'Weight', value: '4', icon: "scale" },
+    { name: 'Volume', value: '5', icon: "view_in_ar" },
+    { name: 'Currency', value: '6', icon: "attach_money" },
+    { name: 'Time', value: '7', icon: "schedule" },
+    { name: 'Speed', value: '8', icon: "speed" },
+    { name: 'Power', value: '9', icon: "bolt" },
+    { name: 'Angle', value: '10', icon: "text_rotation_angleup" },
+    { name: 'Data', value: '11', icon: "leaderboard" },
+    { name: 'Fuel', value: '12', icon: "battery_charging_full" },
   ];
 
   return (
@@ -38,33 +39,40 @@ function App() {
     <>
     <Navbar variant="dark" style={{ backgroundColor: "#3EAD17", height: "150px" }}>
       <Container style={{display: "flex", justifyContent: "center"}}>
-       <p className='navbar-text'>Unit Converter</p>
+       <p className='navbar-text app-font text-center'>Unit Converter</p>
       </Container>
 
     </Navbar>
     <br></br>
-    <div className='d-flex justify-content-center' style={{backgroundColor: "#D9D9D9", overflowX: "scroll"}}>
-      <ButtonGroup className='bg-white'>
-          {radios.map((radio, idx) => (
-            <ToggleButton
-              key={idx}
-              id={`radio-${idx}`}
-              type="radio"
-              variant={'outline-primary'}
-              name="radio"
-              value={radio.value}
-              checked={radioValue === radio.value}
-              onChange={(e) => setRadioValue(e.currentTarget.value)}
-              className='unit-category'
-            >
-              {radio.name}
-            </ToggleButton>
-          ))}
-        </ButtonGroup>
+    <div className='d-flex justify-content-center app-font' style={{backgroundColor: "#D9D9D9"}}>
+      <section style={{overflowX: "scroll"}}>
+        <ButtonGroup className='bg-white'>
+            {radios.map((radio, idx) => (
+              <ToggleButton
+                key={idx}
+                id={`radio-${idx}`}
+                type="radio"
+                variant={'outline-primary'}
+                name="radio"
+                value={radio.value}
+                checked={radioValue === radio.value}
+                onChange={(e) => setRadioValue(e.currentTarget.value)}
+                className='unit-category'
+              >
+                <MaterialIcon icon={radio.icon} size={40}/>
+                <br></br>
+                {radio.name}
+                
+                
+              </ToggleButton>
+              
+            ))}
+          </ButtonGroup>
+        </section>
     </div>
     
     <br></br>
-    <Container className='d-flex justify-content-center'>
+    <Container className='d-flex justify-content-center app-font'>
       <Row>
         <Col xs={12} md={5} className='d-flex justify-content-center'>
           <div className='convert-div d-flex justify-content-center' >
@@ -92,8 +100,11 @@ function App() {
         </Col>
         
         <Col xs={12} md={2} className='d-flex justify-content-center'>
+          
           <div className='switch-units-div'>
-            Col2
+            <Button>
+              <MaterialIcon icon="swap_horiz" size={55}/>
+            </Button>
           </div>
         
         </Col>
@@ -129,7 +140,9 @@ function App() {
     <br></br>
     <br></br>
     <br></br>
-    <div className='footer'></div>
+    <div className='footer'>
+      <p className='app-font'>Developed by <a style={{color: "#3399FF"}} href="https://github.com/cyprste2717218">@cyprste2717218</a></p>
+    </div>
     
     </>
     
