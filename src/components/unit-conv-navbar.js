9aebed64f15cc484be3c54ff5bbe555dfa427308
Stephen Ingham <smi505@york.ac.uk>
2023-12-17 22:24:04 +0000

    removed footer on large devics in place of of canvas element

--- src/components/unit-conv-navbar.js
+++ src/components/unit-conv-navbar.js
@@ -7,8 +7,8 @@ const UnitConvNavbar = ({handleShow}) => {
     return (
         <Navbar variant="dark" className='navbar-div'>
             <Container style={{display: "flex", justifyContent: "flex-start"}}>
-                <FontAwesomeIcon className='icon-large-devices' style={{fontSize: "40px", color: "#fff", marginRight: "15px"}} icon={faScrewdriverWrench} />
-                <FontAwesomeIcon onClick={handleShow} className='icon-small-devices' style={{fontSize: "40px", color: "#fff", marginRight: "15px"}} icon={faBars} />
+                <FontAwesomeIcon onClick={handleShow} className='burger-icon' style={{fontSize: "40px", marginRight: "15px"}} icon={faBars} />
+
                 
                 <div style={{marginTop: "10px"}}>
                     <p className='navbar-text'>Unit Converter</p>
