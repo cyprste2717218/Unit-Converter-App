fcb82bdca2f3c654c983e3e0e4b202eb74f7d25e
Stephen Ingham <smi505@york.ac.uk>
2024-01-06 15:18:53 +0000

    refactored logic for clipboard api

--- src/components/copy-value-button.js
+++ src/components/copy-value-button.js
@@ -7,48 +7,40 @@ const CopyValueButton = ({outputValue}) => {
 
     const [copyButtonTextDisplayed, setCopyButtonTextDisplayed] = useState(false);
 
-    const onCopy = () => {
-
-        // checking if device granted access to copy to clipboard
-        const queryOpts = { name: "clipboard-write", allowWithoutGesture: false};
-       
-        if (navigator.clipboard) {
+    async function copyTextToClipboard(outputValue) {
+        if ('clipboard' in navigator) {
             console.log("navigator has clipboard");
-
-            navigator.permissions.query(queryOpts).then((result) => {
-                console.log("resolved to some result object:", result);
-                if (result.state === "granted" || result.state === "prompt") {
-                // writing output value to clipboard (primary way for most browsers)
-                  console.log("result state either granted or prompt");
-                  navigator.clipboard.writeText(outputValue).then(
-                    () => {
-                        console.log("Copy to clipboard successful!");
-                        setCopyButtonTextDisplayed(true);
-    
-                        setTimeout(() => {
-                            setCopyButtonTextDisplayed(false);
-                        }, 3000)
-                    },
-                    () => {
-                        console.log("Copy to clipboard not succesful");
-                    },
-                  ).catch((err) => {
-                    console.log("Error:", err)
-                  });
-                } else {
-                    console.log("result.state is not granted or prompt");
-                }
-
-              });
-                
-    
+            
+            // writing output value to clipboard (primary way for most browsers)
+            return await navigator.clipboard.writeText(outputValue);
         } else {
             console.log("no navigator.clipboard found");
-
+            
             // writing output value to clipboard (fallback method for older/other browsers)
             document.execCommand('copy', true, outputValue);
-
         }
+    }
+    
+    const onCopy = () => {
+
+        copyTextToClipboard(outputValue)
+        .then(() => {
+            console.log("Copy to clipboard successful!");
+            setCopyButtonTextDisplayed(true);
+
+            setTimeout(() => {
+                setCopyButtonTextDisplayed(false);
+            }, 3000)
+        })
+        .catch((err) => {
+            console.log("Error", err);
+        });
+            
+
+      
+
+          
+
     
 
         
